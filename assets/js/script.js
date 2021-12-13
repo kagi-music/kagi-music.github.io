4db1c0126ea73bdb2371cc9ee08f3976612d629c
kagi-music <80178759+kagi-music@users.noreply.github.com>
2021-12-13 09:29:37 +0700

    commit on 'main'

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,163 +12,112 @@ $(document).ready(function () {
   });
 });
 
-// Open Search Bar
-document.querySelector('nav .search-btn').addEventListener('click', () => {
-  document.querySelector('nav .search-container').classList.add('show');
-});
+// Counter
+var i;
 
-document.querySelector('nav .search-container button').addEventListener('click', () => {
-  document.querySelector('nav .search-container').classList.remove('show');
-});
+// Open Search Bar
+const navSearchBtn = document.querySelector('nav .search-btn');
+const navSearchContainer = document.querySelector('nav .search-container');
+const navSearchContainerBtn = document.querySelector('nav .search-container button');
+navSearchBtn.addEventListener('click', () =>  navSearchContainer.classList.add('show'));
+navSearchContainerBtn.addEventListener('click', () => navSearchContainer.classList.remove('show'));
 
 // Search List
 document.getElementById('search-box').addEventListener('keyup', () => {
-  var input, keyword, ul, li, a, i;
-  input = document.getElementById('search-box');
-  keyword = input.value.toLowerCase();
-  ul = document.getElementById('search-list');
-  li = document.querySelectorAll('nav .search-list li');
-
+  let input = document.getElementById('search-box');
+  const keyword = input.value.toLowerCase();
+  let ul = document.getElementById('search-list');
+  let li = document.querySelectorAll('nav .search-list li');
   if (input.value.length == 0) {
     ul.style.display = 'none';
     return 0;
   } else {
     ul.style.display = 'block';
   }
-
   for (i = 0; i < li.length; i++) {
-    a = li[i].querySelectorAll('nav .search-list a')[0];
-    if (a.innerHTML.toLowerCase().indexOf(keyword) > -1) {
-      li[i].style.display = 'block';
-    } else {
-      li[i].style.display = 'none';
-    }
+    let a = li[i].querySelectorAll('nav .search-list a')[0];
+    a.innerHTML.toLowerCase().indexOf(keyword) > -1 ? li[i].style.display = 'block' : li[i].style.display = 'none';
   }
 });
 
 // Language
-let jpmode = localStorage.getItem('JPMode');
-const langbtn = document.getElementById('lang-btn');
-var en = document.getElementsByClassName('en');
-var jp = document.getElementsByClassName('jp');
-var original = document.querySelectorAll('.original h4');
-var cover = document.querySelectorAll('.cover h4');
+let jpMode = localStorage.getItem('JPMode');
+const langBtn = document.getElementById('lang-btn');
+const en = document.getElementsByClassName('en');
+const jp = document.getElementsByClassName('jp');
+const original = document.querySelectorAll('.original h4');
+const cover = document.querySelectorAll('.cover h4');
 
 function enableJPMode() {
   localStorage.setItem('JPMode', 'enabled');
-  langbtn.innerHTML = '日本語';
-  for (i = 0; i < en.length; i++) {
-    en[i].style.display = 'none';
-  }
-  for (i = 0; i < jp.length; i++) {
-    jp[i].style.display = 'block';
-  }
-  for (i = 0; i < original.length; i++) {
-    original[i].innerHTML = 'オリジナル';
-  }
-  for (i = 0; i < cover.length; i++) {
-    cover[i].innerHTML = 'カバー';
-  }
+  langBtn.innerHTML = '日本語';
+  for (i = 0; i < en.length; i++) en[i].style.display = 'none';
+  for (i = 0; i < jp.length; i++) jp[i].style.display = 'block';
+  for (i = 0; i < original.length; i++) original[i].innerHTML = 'オリジナル';
+  for (i = 0; i < cover.length; i++) cover[i].innerHTML = 'カバー';
 }
 
 function disableJPMode() {
   localStorage.setItem('JPMode', null);
-  langbtn.innerHTML = 'English';
-  for (i = 0; i < en.length; i++) {
-    en[i].style.display = 'block';
-  }
-  for (i = 0; i < jp.length; i++) {
-    jp[i].style.display = 'none';
-  }
-  for (i = 0; i < original.length; i++) {
-    original[i].innerHTML = 'Original';
-  }
-  for (i = 0; i < cover.length; i++) {
-    cover[i].innerHTML = 'Cover';
-  }
+  langBtn.innerHTML = 'English';
+  for (i = 0; i < en.length; i++) en[i].style.display = 'block';
+  for (i = 0; i < jp.length; i++) jp[i].style.display = 'none';
+  for (i = 0; i < original.length; i++) original[i].innerHTML = 'Original';
+  for (i = 0; i < cover.length; i++) cover[i].innerHTML = 'Cover';
 }
 
-if (jpmode === 'enabled') {
-  enableJPMode();
-} else {
-  disableJPMode();
-}
+jpMode === 'enabled' ? enableJPMode() : disableJPMode();
 
 document.getElementById('lang-btn').addEventListener('click', () => {
-  jpmode = localStorage.getItem('JPMode');
-  if (jpmode != 'enabled') {
-    enableJPMode();
-  } else {
-    disableJPMode();
-  }
+  jpMode = localStorage.getItem('JPMode');
+  jpMode != 'enabled' ? enableJPMode() : disableJPMode();
 });
 
 // Dark Mode
-var i, menu, searchbox, searchlist, container, card, downloadlist;
-let darkmode = localStorage.getItem('DarkMode');
-const darkmodeToggle = document.getElementById('theme-btn');
-menu = document.getElementById('menu-overlay');
-searchbox = document.querySelector('nav .search-container input');
-searchlist = document.getElementById('search-list');
-container = document.getElementsByClassName('container');
-card = document.getElementsByClassName('card');
-downloadlist = document.getElementsByClassName('download-list');
+let darkMode = localStorage.getItem('DarkMode');
+const darkModeToggle = document.getElementById('theme-btn');
+const menu = document.getElementById('menu-overlay');
+const searchBox = document.querySelector('nav .search-container input');
+const searchList = document.getElementById('search-list');
+const container = document.getElementsByClassName('container');
+const card = document.getElementsByClassName('card');
+const downloadList = document.getElementsByClassName('download-list');
 
 function enableDarkMode() {
   localStorage.setItem('DarkMode', 'enabled');
-  darkmodeToggle.innerHTML = '<i class="fas fa-moon"></i>';
-  darkmodeToggle.style.transform = 'scaleX(-1)';
+  darkModeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+  darkModeToggle.style.transform = 'scaleX(-1)';
   document.body.classList.add('dark');
   menu.classList.add('dark');
-  searchbox.classList.add('dark');
-  searchlist.classList.add('dark');
-  for (i = 0; i < container.length; i++) {
-    container[i].classList.add('dark');
-  }
-  for (i = 0; i < card.length; i++) {
-    card[i].classList.add('dark');
-  }
-  for (i = 0; i < downloadlist.length; i++) {
-    downloadlist[i].classList.add('dark');
-  }
+  searchBox.classList.add('dark');
+  searchList.classList.add('dark');
+  for (i = 0; i < container.length; i++) container[i].classList.add('dark');
+  for (i = 0; i < card.length; i++) card[i].classList.add('dark');
+  for (i = 0; i < downloadList.length; i++) downloadList[i].classList.add('dark');
 }
 
 function disableDarkMode() {
   localStorage.setItem('DarkMode', null);
-  darkmodeToggle.innerHTML = '<i class="fas fa-sun"></i>';
-  darkmodeToggle.style.transform = 'scaleX(1)';
+  darkModeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+  darkModeToggle.style.transform = 'scaleX(1)';
   document.body.classList.remove('dark');
   menu.classList.remove('dark');
-  searchbox.classList.remove('dark');
-  searchlist.classList.remove('dark');
-  for (i = 0; i < container.length; i++) {
-    container[i].classList.remove('dark');
-  }
-  for (i = 0; i < card.length; i++) {
-    card[i].classList.remove('dark');
-  }
-  for (i = 0; i < downloadlist.length; i++) {
-    downloadlist[i].classList.remove('dark');
-  }
+  searchBox.classList.remove('dark');
+  searchList.classList.remove('dark');
+  for (i = 0; i < container.length; i++) container[i].classList.remove('dark');
+  for (i = 0; i < card.length; i++) card[i].classList.remove('dark');
+  for (i = 0; i < downloadList.length; i++) downloadList[i].classList.remove('dark');
 }
 
-if (darkmode === 'enabled') {
-  enableDarkMode();
-}
+if (darkMode === 'enabled') enableDarkMode();
 
 document.getElementById('theme-btn').addEventListener('click', () => {
-  darkmode = localStorage.getItem('DarkMode');
-  if (darkmode != 'enabled') {
-    enableDarkMode();
-  } else {
-    disableDarkMode();
-  }
+  darkMode = localStorage.getItem('DarkMode');
+  darkMode != 'enabled' ? enableDarkMode() : disableDarkMode();
 });
 
 // Open Menu Overlay
-document.getElementById('menu-btn').addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
+document.getElementById('menu-btn').addEventListener('click', () => menu.classList.toggle('show'));
 
 // Download
 document.querySelector('.download-btn').addEventListener('click', () => {
@@ -177,7 +126,7 @@ document.querySelector('.download-btn').addEventListener('click', () => {
 });
 
 // Character
-var chara = document.getElementById('character');
+const chara = document.getElementById('character');
 function character(num) {
   var num = Math.floor(Math.random() * img.length);
   chara.src = img[num];
